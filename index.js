729ed31712408817eb9347eb48e4412212299d55
chiaki-kjwr <chiakikajiwara@gmail.com>
2020-08-02 09:55:49 +0900

    async関数でAPIを取得

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-function callApi(){
+async function callApi(){
   //APIを叩く処理
   //APIを叩いて、かえってきたデータ
-  const res = window.fetch("https://jsonplaceholder.typicode.com/users");
-  console.log(res);
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const users = await res.json();
+  console.log(users);
 }
 callApi();
